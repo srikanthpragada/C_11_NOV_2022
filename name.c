72ab6ec04af8593d7e440fabef2094231727df25
srikanthpragada <srikanthpragada@gmail.com>
2022-11-15 18:43:16 +0530

    15-NOV

---
+++ name.c
@@ -0,0 +1,10 @@
+// Program to print name and mobile number
+// Date : 15-NOV-2022
+
+#include <stdio.h>
+
+void main()
+{
+   printf("Srikanth Technologies\n");
+   printf("9059057000");
+}
---
+++ square.c
@@ -0,0 +1,14 @@
+// Program to print square of a number
+// Date : 15-NOV-2022
+
+#include <stdio.h>
+
+void main()
+{
+  int num, square; // variables
+
+      num = 53;  // assignment
+      square = num * num;
+
+      printf("Square of %d is %d", num, square);
+}
